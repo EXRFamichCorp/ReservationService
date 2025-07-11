e8452af5a8187195ee7c7683ad3860b32fc7bd33
EXREGISTR <115467831+EXREGISTR@users.noreply.github.com>
2025-07-12 01:28:11 +0300

    убрал лишнюю хрень и добавил другую хрень

--- Contracts/obj/Debug/net9.0/Contracts.AssemblyInfo.cs
+++ Contracts/obj/Debug/net9.0/Contracts.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("API")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa84f589247867dfc8478ef056b782f56347c830")]
-[assembly: System.Reflection.AssemblyProductAttribute("API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("API")]
+[assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
